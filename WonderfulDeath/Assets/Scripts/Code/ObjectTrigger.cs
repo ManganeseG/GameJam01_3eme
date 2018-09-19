22e634b8aab2435d796a15c48485c9b0941c23c2
ManganeseG <celinebrasseurcbr@gmail.com>
2018-09-19 12:56:38 +0200

    Wonderful Death import

---
+++ WonderfulDeath/Assets/Scripts/Code/ObjectTrigger.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectTrigger : MonoBehaviour
+{
+    public enum ListOfTrigger
+    {
+        Tool,
+        Bucket,
+        Door,
+        Press
+    };
+    public ListOfTrigger TriggerType;
+
+    public Text TextTool;
+    public Text TextBucket;
+    public Text TextDoor;
+    public Text TextPress;
+    private Text textToTrigger;
+
+    private bool gotItem = false;
+    [HideInInspector]public bool openDoor = false;
+
+    public GameObject LeftDoor;
+    public GameObject RightDoor;
+    private Animator leftDoorAnim;
+    private Animator rightDoorAnim;
+
+    public float Countdown;
+    private float countdown;
+    private bool startCountdown = false;
+
+    void Start()
+    {
+        countdown = Countdown;
+
+        TextTool.enabled = false;
+        TextBucket.enabled = false;
+        TextDoor.enabled = false;
+        TextPress.enabled = false;
+
+        gotItem = false;
+
+        TypeOfTrigger();
+
+        if (LeftDoor || RightDoor == null)
+            return;
+    }
+
+    void Update()
+    {
+        if(gotItem)
+        {
+           textToTrigger.enabled = true;
+            if (TriggerType == ListOfTrigger.Door)
+            {
+                startCountdown = true;
+                leftDoorAnim.SetBool("Open", true);
+                rightDoorAnim.SetBool("Open", true);
+                if(countdown <= 0)
+                {
+                    textToTrigger.enabled = false;
+                    gotItem = false;
+                    startCountdown = false;
+                    countdown = Countdown;
+                    leftDoorAnim.SetBool("Open", false);
+                    rightDoorAnim.SetBool("Open", false);
+                }
+            }
+
+            if (TriggerType == ListOfTrigger.Bucket && TextTool.enabled == true)
+            {
+                TextTool.enabled = false;
+            }
+             if (TriggerType == ListOfTrigger.Tool && TextBucket.enabled == true)
+            {
+                TextBucket.enabled = false;
+            }
+            if (Input.GetButton("Fire2") && (TriggerType == ListOfTrigger.Tool || TriggerType == ListOfTrigger.Bucket))
+           {
+               gotItem = false;
+               textToTrigger.enabled = false;
+           }
+        }
+
+        if(startCountdown)
+        {
+            countdown -= Time.deltaTime;
+        }
+    }
+    
+    private void TypeOfTrigger()
+    {
+        switch (TriggerType)
+        {
+            case ListOfTrigger.Tool:
+                textToTrigger = TextTool;
+                break;
+            case ListOfTrigger.Bucket:
+                textToTrigger = TextBucket;
+                break;
+            case ListOfTrigger.Door:
+                textToTrigger = TextDoor;
+                leftDoorAnim = LeftDoor.GetComponent<Animator>();
+                rightDoorAnim = RightDoor.GetComponent<Animator>();
+                break;
+            case ListOfTrigger.Press:
+                textToTrigger = TextPress;
+                break;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player") && Input.GetButton("Fire1"))
+        {
+            gotItem = true;
+        }
+    }
+}
---
+++ WonderfulDeath/Assets/Scripts/Code/PlayerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public float Speed = 1f;
+    public float horizontal;
+    public float vertical;
+    public float turnSmoothing = 10f;
+
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        horizontal *= Speed;
+        vertical = Input.GetAxis("Vertical");
+        vertical *= Speed;
+
+        PlayerControl(horizontal, vertical);
+
+
+        transform.position += new Vector3(horizontal, 0f, vertical);
+
+    }
+
+    void PlayerControl(float horizontal, float vertical)
+    {
+        if(horizontal != 0f || vertical != 0f)
+        {
+            Vector3 targetDirection = new Vector3(horizontal, 0f, vertical);
+
+            
+            Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+            Quaternion newRotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSmoothing * Time.deltaTime);
+            transform.rotation = newRotation;
+        }
+    }
+}
